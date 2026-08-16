25b193ce8d7000f6d19cd0640999d36bff3a708f
Prashu Chauhan <prashu2764@email.com>
2026-08-16 15:04:18 +0530

    Added manifest.js and sw.js for room scout app

---
+++ Room_Scout/manifest.js
@@ -0,0 +1,21 @@
+{
+  "name": "Room Scout",
+  "short_name": "Room Scout",
+  "description": "Saved the free available rooms",
+  "start_url": "./",
+  "display": "standalone",
+  "background_color": "#ffffff",
+  "theme_color": "#ffffff",
+  "icons": [
+    {
+      "src": "icons/icon-192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/icon-512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
